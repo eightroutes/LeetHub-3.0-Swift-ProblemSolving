a19d2a5602a30c660b2aea6a43e48f6b777f4fea
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-02 16:26:39 +0900

    Time: 0 ms (100%), Space: 20.3 MB (24.86%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.swift
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.swift
@@ -1,18 +1,25 @@
 class Solution {
     func gcdOfStrings(_ str1: String, _ str2: String) -> String {
-        if str1 + str2 != str2 + str1 {
-            return ""
+        let minCount = min(str1.count, str2.count)
+        let len1 = str1.count
+        let len2 = str2.count
+
+        func isValid(_ k: Int) -> Bool {
+            if (len1 % k != 0) || (len2 % k != 0) {
+                return false
+            } 
+            let base = String(str1.prefix(k))
+            let repStr1 = String(repeating: base, count: len1 / k)
+            let repStr2 = String(repeating: base, count: len2 / k) 
+
+            return (str1 == repStr1) && (str2 == repStr2)
         }
 
-        func gcd(_ x: Int, _ y: Int) -> Int {
-            if y == 0 {
-                return x
-            } else {
-            return gcd(y, x % y)
+        for i in stride(from: minCount, to: 0, by: -1) {
+            if isValid(i) {
+                return String(str1.prefix(i))
             }
         }
-
-        let gcdLength = gcd(str1.count, str2.count)
-        return String(str1.prefix(gcdLength))
-    }    
+        return ""
+    }
 }
