2661bda55b459013bb0feea950c99cdbb6cc6181
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-04 16:02:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.swift
+++ 0605-can-place-flowers/0605-can-place-flowers.swift
@@ -2,12 +2,13 @@ class Solution {
     func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
         var count = 0
         var flowerbed = flowerbed
+
         for i in 0..<flowerbed.count {
             if flowerbed[i] == 0 {
-                let isEmptyLeft = (i == 0) || (flowerbed[i-1] == 0)
-                let isEmptyRight = (i == flowerbed.count-1) || (flowerbed[i+1] == 0)
-                
-                if isEmptyLeft && isEmptyRight {
+                let isLeftEmpty = (i==0) || (flowerbed[i-1]==0)
+                let isRightEmpty = (i==flowerbed.count-1) || (flowerbed[i+1]==0)
+
+                if isLeftEmtpy && isRightEmpty {
                     flowerbed[i] = 1
                     count += 1
                     if count >= n {
