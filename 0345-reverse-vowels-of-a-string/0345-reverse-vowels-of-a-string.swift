32d8d7b81282d493b5f23d8a12cbe1f084032ba6
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-04 17:42:28 +0900

    Time: 953 ms (5.84%), Space: 22.3 MB (10.06%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.swift
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.swift
@@ -1,16 +1,16 @@
 class Solution {
     func reverseVowels(_ s: String) -> String {
-        let vowels = ["a", "e", "i", "o", "u"]
+        let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
         var vowelArr = [Character]()
-        var sArr = s.map { $0 }
+        var sArr = Array(s)
 
         for i in 0..<sArr.count {
-            if vowels.contains(String(sArr[i]).lowercased()) {
+            if vowels.contains(sArr[i].lowercased()) {
                 vowelArr.append(sArr[i])
             }
         }
         for i in 0..<sArr.count {
-            if vowels.contains(String(sArr[i]).lowercased()) {
+            if vowels.contains(sArr[i].lowercased()) {
                 if !vowelArr.isEmpty {
                     sArr[i] = vowelArr.popLast()!
                 } else {
