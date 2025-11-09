f4acb482aa5d7dfb1378cf246625722e88c4ba3d
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-09 12:34:56 +0900

    Time: 1413 ms (7.47%), Space: 22.6 MB (10.39%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.swift
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.swift
@@ -1,13 +1,13 @@
 class Solution {
     func findMaxAverage(_ nums: [Int], _ k: Int) -> Double {
         var currentSum = nums[0..<k].reduce(0, +)
-        var maxSum = currentSum 
+        var maxSum = currentSum
+        print(currentSum)
         for i in k..<nums.count {
             currentSum = currentSum - nums[i-k] + nums[i]
-            if currentSum > maxSum {
-                maxSum = currentSum
-            }
+            maxSum = max(maxSum, currentSum)
+            print(maxSum)
         }
-        return Double(maxSum) / Double(k)   
+        return Double(maxSum) / Double(k)
     }
 }
