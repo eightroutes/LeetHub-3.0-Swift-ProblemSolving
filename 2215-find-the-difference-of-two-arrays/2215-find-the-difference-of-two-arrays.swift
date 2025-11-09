1a5feb66c11679795231207eb8c2885898db0612
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-09 12:49:06 +0900

    Time: 20 ms (13.27%), Space: 19.9 MB (79.65%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.swift
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.swift
@@ -1,12 +1,23 @@
 class Solution {
     func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
         
+        var result = Array(repeating: [Int](), count: 2)
+    
         let set1 = Set(nums1)
         let set2 = Set(nums2)
         
-        let subtract1 = set1.subtracting(set2)
-        let subtract2 = set2.subtracting(set1)
+        for num in set1 {
+            if !set2.contains(num) {
+                result[0].append(num)
+            }
+        }
         
-        return [Array(subtract1), Array(subtract2)]
+        for num in set2 {
+            if !set1.contains(num) {
+                result[1].append(num)
+            }
+        }
+        
+        return result
     }
 }
