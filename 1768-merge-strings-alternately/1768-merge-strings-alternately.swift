4e8e9bf80851b2d8f0f546605ff6574b25e59996
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-14 12:37:33 +0900

    Time: 7 ms (45.05%), Space: 18.9 MB (43.81%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.swift
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.swift
@@ -1,22 +1,18 @@
 class Solution {
     func mergeAlternately(_ word1: String, _ word2: String) -> String {
+        
         var result = ""
 
-        let count = max(word1.count, word2.count)
-        print(count)
-
-        for i in 0...count {
-            if i < word1.count {
-                let index1 = word1.index(word1.startIndex, offsetBy: i)
-                result.append(word1[index1])
-            }
-            if i < word2.count {
-                let index2 = word2.index(word2.startIndex, offsetBy: i)
-                result.append(word2[index2])
-            }
+        for (c1, c2) in zip(word1, word2) {
+            result += [c1, c2]
         }
 
-
+        if word1.count < word2.count {
+            result += word2[word1.endIndex...]
+        } else {
+            result += word1[word2.endIndex...]
+        }
+        
         return result
     }
 }
