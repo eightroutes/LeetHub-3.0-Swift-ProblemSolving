c2754c61e6b6a97122c61c8769dd73f184a40f55
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-17 08:37:38 +0900

    Time: 108 ms (12.31%), Space: 27.2 MB (15.38%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.swift
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.swift
@@ -10,23 +10,37 @@
  */
 class Solution {
     func pairSum(_ head: ListNode?) -> Int {
-        var values = [Int]()
-        var current = head
+        // 1. slow/fast로 중간 찾기
+        var slow = head, fast = head
+        
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        // slow가 뒤쪽 절반의 시작점을 가리킴
+        
+        // 2. 뒤쪽 절반 뒤집기
+        var prev: ListNode? = nil
+        var current = slow
         
         while current != nil {
-            values.append(current!.val)
-            current = current?.next
+            let nextNode = current?.next
+            current?.next = prev
+            prev = current
+            current = nextNode
         }
+        // 이제 prev가 뒤집힌 뒤쪽 절반의 head
         
-        var start = 0, end = values.count-1
-        var maxValue = 0
-        while start < end {
-            let twin = values[start] + values[end]
-            maxValue = max(maxValue, twin)
-            start += 1
-            end -= 1
+        // 3. 앞쪽과 뒤쪽 동시 순회화며 최댓값 찾기
+        var front = head, back = prev
+        var maxSum = 0
+        
+        while back != nil {
+            let twinSum = front!.val + back!.val
+            maxSum = max(maxSum, twinSum)
+            front = front?.next
+            back = back?.next
         }
-        
-        return maxValue
+        return maxSum
     }
 }
