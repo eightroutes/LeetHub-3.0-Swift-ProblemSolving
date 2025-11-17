9c8ca01f5b37b7b45c452f82ba4a9530640d1486
GEUNHO <52287330+eightroutes@users.noreply.github.com>
2025-11-17 11:32:52 +0900

    Time: 0 ms (100%), Space: 19.5 MB (60%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.swift
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.swift
@@ -15,24 +15,25 @@
  */
 class Solution {
     func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
-    
-        var leaves1 = [Int]()
-        var leaves2 = [Int]()
-        
-        collect(root1, &leaves1)
-        collect(root2, &leaves2)
-        
-        return leaves1 == leaves2
+        return getLeaves(root1) == getLeaves(root2)
     }
 
-    func collect(_ node: TreeNode?, _ leaves: inout [Int]) {
-        guard let node else { return }
-        
-        collect(node.left, &leaves)
-        collect(node.right, &leaves)
-        
-        if node.left == nil && node.right == nil {
-            leaves.append(node.val)
+    func getLeaves(_ root: TreeNode?) -> [Int] {
+        guard let root else { return [] }
+
+        var stack: [TreeNode] = [root]
+        var result: [Int] = []
+
+        while !stack.isEmpty {
+            let node = stack.removeLast()
+
+            if node.left == nil && node.right == nil {
+                result.append(node.val)
+            }
+
+            if let right = node.right { stack.append(right) }
+            if let left = node.left { stack.append(left) }
         }
+        return result
     }
 }
